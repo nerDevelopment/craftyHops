bb8c19dd65f4bda8ed59b2be4bc33874ece8c2d1
Jessica <swampgliders@gmail.com>
2012-11-29 16:30:25 -0500

    validate css/html

--- inc/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  var toggleBtn = $("#navbtn");
-  if (toggleBtn.length > 0) {
-  $("#navbtn").click(function(){
-      $("body").toggleClass("active");
-      	return false;
-    });
-  }
-});
